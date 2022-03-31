567949e6caccf5cd6c54af70ce90e9e4bf745661
Luotianxu <1265062404@qq.com>
2022-03-31 19:55:29 +0800

    NProgress

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,9 @@
 import {createRouter, createWebHashHistory, RouteRecordRaw} from 'vue-router'
 import systemRouter from '@/router/modules/system'
 import demoRouter from '@/router/modules/demo'
+import NProgress from 'nprogress'
+import 'nprogress/css/nprogress.css'
+NProgress.configure({ showSpinner: false })
 
 export const asyncRouterList: Array<RouteRecordRaw> = [...systemRouter, ...demoRouter]
 
@@ -21,6 +24,15 @@ const routes: RouteRecordRaw[] = [...baseRouter, ...asyncRouterList]
 const router = createRouter({
     routes: routes,
     history: createWebHashHistory()
+})
+
+router.beforeEach((to, from, next) => {
+    NProgress.start()
+    next()
+})
+
+router.afterEach(() => {
+    NProgress.done()
 })
 
 export default router
