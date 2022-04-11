41b8817bb02529702ac4cf6a890f02ebdd0610e4
Luotianxu <1265062404@qq.com>
2022-04-11 10:17:45 +0800

    hooks

--- components.d.ts
+++ components.d.ts
@@ -25,6 +25,7 @@ declare module 'vue' {
     ElForm: typeof import('element-plus/es')['ElForm']
     ElFormItem: typeof import('element-plus/es')['ElFormItem']
     ElIcon: typeof import('element-plus/es')['ElIcon']
+    ElIconEdit: typeof import('element-plus/es')['ElIconEdit']
     ElInput: typeof import('element-plus/es')['ElInput']
     ElLink: typeof import('element-plus/es')['ElLink']
     ElMenu: typeof import('element-plus/es')['ElMenu']
