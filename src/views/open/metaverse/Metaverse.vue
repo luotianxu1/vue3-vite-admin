3312047d89807755d74c7ae8d54d647ca8d0ea3c
luotianxu <1265062404@qq.com>
2022-09-13 21:50:31 +0800

    three

--- src/views/open/metaverse/Metaverse.vue
+++ src/views/open/metaverse/Metaverse.vue
@@ -5,12 +5,12 @@
 <script setup lang="ts">
     import * as THREE from 'three'
     import Stats from 'three/examples/jsm/libs/stats.module.js'
-    import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
     import { Octree } from 'three/examples/jsm/math/Octree'
-    import { OctreeHelper } from 'three/examples/jsm/helpers/OctreeHelper'
     import { Capsule } from 'three/examples/jsm/math/Capsule'
 
     onMounted(() => {
+        const clock = new THREE.Clock()
+
         const scene = new THREE.Scene()
         scene.background = new THREE.Color(0x88ccee)
         scene.fog = new THREE.Fog(0x88ccee, 0, 50)
@@ -36,26 +36,20 @@
         const stats = Stats()
         stats.domElement.style.position = 'absolute'
         stats.domElement.style.top = '0px'
-
         if (!container) {
             return
         }
-        container.appendChild(renderer.domElement)
         container.appendChild(stats.domElement)
+        container.appendChild(renderer.domElement)
 
-        const controls = new OrbitControls(camera, renderer.domElement)
-        controls.target.set(0, 0, 0)
-
-        const clock = new THREE.Clock()
         function animate() {
             let delta = clock.getDelta()
-            // 计算玩家的方向向量
-            controlsPlayer(delta)
-            // 更新人物状态
+            // console.log(delta);
+            controlPlayer(delta)
             updatePlayer(delta)
-            // 重置
             resetPlayer()
             stats.update()
+            // controls.update();
             renderer.render(scene, camera)
             requestAnimationFrame(animate)
         }
@@ -70,13 +64,22 @@
         plane.receiveShadow = true
         plane.rotation.x = -Math.PI / 2
 
+        // 创建立方体叠楼梯效果
+        for (let i = 0; i < 10; i++) {
+            const boxGeometry = new THREE.BoxGeometry(1,1,0.1)
+            const boxMaterial = new THREE.MeshBasicMaterial({color: 0x00ff00})
+            const box = new THREE.Mesh(boxGeometry,boxMaterial)
+            box.position.y = 0.15 + i * 0.15
+            box.position.z = i * 0.3
+            plane.add(box)
+        }
+
         // 创建一个octree
         const group = new THREE.Group()
         group.add(plane)
         scene.add(group)
-
-        const wordOctree = new Octree()
-        wordOctree.fromGraphNode(group)
+        const worldOctree = new Octree()
+        worldOctree.fromGraphNode(group)
 
         // 创建一个人的碰撞体
         const playerCollider = new Capsule(
@@ -84,6 +87,19 @@
             new THREE.Vector3(0, 1.35, 0),
             0.35
         )
+
+        // 创建一个平面
+        const capsuleBodyGeometry = new THREE.PlaneGeometry(1, 0.5, 1, 1)
+        const capsuleBodyMaterial = new THREE.MeshBasicMaterial({
+            color: 0x0000ff,
+            side: THREE.DoubleSide
+        })
+        const capsuleBody = new THREE.Mesh(
+            capsuleBodyGeometry,
+            capsuleBodyMaterial
+        )
+        capsuleBody.position.set(0, 0.5, 0)
+
         // 创建一个胶囊物体
         const capsuleGeometry = new THREE.CapsuleGeometry(0.35, 1, 32)
         const capsuleMaterial = new THREE.MeshBasicMaterial({
@@ -91,18 +107,21 @@
             side: THREE.DoubleSide
         })
         const capsule = new THREE.Mesh(capsuleGeometry, capsuleMaterial)
-        capsule.position.set(0, 0.85, 1)
-        capsule.castShadow = true
+        capsule.position.set(0, 0.85, 0)
         scene.add(capsule)
+
+        // 将相机作为胶囊的子元素，就可以实现跟随
+        camera.position.set(0, 2, -5)
+        camera.lookAt(capsule.position)
+        capsule.add(camera)
+        capsule.add(capsuleBody)
 
         // 设置重力
         const gravity = -9.8
-        // 玩家速度
+        // 玩家的速度
         const playerVelocity = new THREE.Vector3(0, 0, 0)
         // 方向向量
         const playerDirection = new THREE.Vector3(0, 0, 0)
-        // 玩家是否在地面上
-        let playerOnFloor = false
         // 键盘按下事件
         const keyStates = {
             KeyW: false,
@@ -113,30 +132,33 @@
             isDown: false
         }
 
+        // 玩家是否在地面上
+        let playerOnFloor = false
+
         function updatePlayer(deltaTime) {
             let damping = -0.05
             if (playerOnFloor) {
                 playerVelocity.y = 0
-                keyStates['isDown'] || playerVelocity.addScaledVector(playerVelocity,damping)
+                keyStates.isDown ||
+                    playerVelocity.addScaledVector(playerVelocity, damping)
             } else {
                 playerVelocity.y += gravity * deltaTime
             }
-            // 计算玩家移动距离
+            // 计算玩家移动的距离
             const playerMoveDistance = playerVelocity
                 .clone()
                 .multiplyScalar(deltaTime)
             playerCollider.translate(playerMoveDistance)
             // 将胶囊的位置进行设置
             playerCollider.getCenter(capsule.position)
-
-            // 碰撞检测
+            // 进行碰撞检测
             playerCollisions()
-
         }
 
         // 人物碰撞检测
         function playerCollisions() {
-            const result = wordOctree.capsuleIntersect(playerCollider)
+            const result = worldOctree.capsuleIntersect(playerCollider)
+            playerOnFloor = false
             if (result) {
                 playerOnFloor = result.normal.y > 0
                 playerCollider.translate(
@@ -160,7 +182,7 @@
             'keydown',
             (event) => {
                 keyStates[event.code] = true
-                keyStates['isDown'] = true
+                keyStates.isDown = true
             },
             false
         )
@@ -168,22 +190,79 @@
             'keyup',
             (event) => {
                 keyStates[event.code] = false
-                keyStates['isDown'] = false
+                keyStates.isDown = false
             },
             false
         )
+        document.addEventListener('mousedown', () => {
+            // 锁定鼠标指针
+            document.body.requestPointerLock()
+        })
 
         // 根据键盘状态更新玩家的速度
-        function controlsPlayer(deltaTime) {
+        function controlPlayer(deltaTime) {
             if (keyStates['KeyW']) {
-                playerVelocity.z = 1
-                // 获取胶囊的正前面方向
-                const capsuleFront = new THREE.Vector3(0,0,0)
+                playerDirection.z = 1
+                //获取胶囊的正前面方向
+                const capsuleFront = new THREE.Vector3(0, 0, 0)
                 capsule.getWorldDirection(capsuleFront)
-                // 计算玩家速度
+                // console.log(capsuleFront);
+                // 计算玩家的速度
                 playerVelocity.add(capsuleFront.multiplyScalar(deltaTime))
             }
+            if (keyStates['KeyS']) {
+                playerDirection.z = 1
+                //获取胶囊的正前面方向
+                const capsuleFront = new THREE.Vector3(0, 0, 0)
+                capsule.getWorldDirection(capsuleFront)
+                // console.log(capsuleFront);
+                // 计算玩家的速度
+                playerVelocity.add(capsuleFront.multiplyScalar(-deltaTime))
+            }
+            if (keyStates['KeyA']) {
+                playerDirection.x = 1
+                //获取胶囊的正前面方向
+                const capsuleFront = new THREE.Vector3(0, 0, 0)
+                capsule.getWorldDirection(capsuleFront)
+
+                // 侧方的方向，正前面的方向和胶囊的正上方求叉积，求出侧方的方向
+                capsuleFront.cross(capsule.up)
+                // console.log(capsuleFront);
+                // 计算玩家的速度
+                playerVelocity.add(capsuleFront.multiplyScalar(-deltaTime))
+            }
+            if (keyStates['KeyD']) {
+                playerDirection.x = 1
+                //获取胶囊的正前面方向
+                const capsuleFront = new THREE.Vector3(0, 0, 0)
+                capsule.getWorldDirection(capsuleFront)
+
+                // 侧方的方向，正前面的方向和胶囊的正上方求叉积，求出侧方的方向
+                capsuleFront.cross(capsule.up)
+                // console.log(capsuleFront);
+                // 计算玩家的速度
+                playerVelocity.add(capsuleFront.multiplyScalar(deltaTime))
+            }
+            if (keyStates['Space']) {
+                playerVelocity.y = 15
+            }
         }
+
+        // 根据鼠标在屏幕移动，来旋转胶囊
+        window.addEventListener('mousemove', (event) => {
+            capsule.rotation.y -= event.movementX * 0.001
+        })
+
+        // 多层次细节展示
+        let lod = new THREE.LOD()
+        const material = new THREE.MeshBasicMaterial({color: 0xff0000,wireframe: true})
+        for (let i = 0; i < 5; i++) {
+            const geometry = new THREE.SphereGeometry(1,22 - i * 5,22 - i * 5)
+            const mesh = new THREE.Mesh(geometry,material)
+            lod.addLevel(mesh, i * 5)
+        }
+        lod.position.set(10,0,10)
+        scene.add(lod)
 
         animate()
     })
