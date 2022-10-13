244ca38a96415d2e91d3a1b4cb515a857acd3e42
luotianxu <1265062404@qq.com>
2022-10-13 12:45:57 +0800

    refactor: 多语言

--- src/lang/index.ts
+++ src/lang/index.ts
@@ -1,16 +1,16 @@
 import { createI18n } from 'vue-i18n'
-import zh from './zh'
-import en from './en'
+import zh from './modules/zh'
+import en from './modules/en'
 
 // 注册i8n实例并引入语言文件
 const i18n = createI18n({
-  legacy: false,
-  globalInjection: true,
-  locale: 'zh', // 默认显示的语言
-  messages: {
-    zh: zh, // 引入语言文件
-    en: en
-  }
+    legacy: false,
+    globalInjection: true,
+    locale: 'zh', // 默认显示的语言
+    messages: {
+        zh: zh, // 引入语言文件
+        en: en
+    }
 })
 
 export default i18n
