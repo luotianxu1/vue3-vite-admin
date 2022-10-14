e23e44bf2f01a6d58437dc8663797308522b1e86
Luotianxu <1265062404@qq.com>
2022-10-14 17:25:07 +0800

    refactor: vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,57 +1,22 @@
-import { defineConfig, loadEnv } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import AutoImport from 'unplugin-auto-import/vite'
-import Components from 'unplugin-vue-components/vite'
-import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
-import path from 'path'
-import cesium from 'vite-plugin-cesium'
-const config = loadEnv('development', './')
+import { defineConfig } from 'vite'
 
-export default defineConfig({
-    plugins: [
-        vue(),
-        AutoImport({
-            resolvers: [ElementPlusResolver()],
-            imports: ['vue', 'vue-router'],
-            dts: 'src/types/auto-imports.d.ts'
-        }),
-        Components({
-            resolvers: [ElementPlusResolver()],
-            // dirs: ['src/components'],
-            // directoryAsNamespace: true,
-            dts: 'src/types/components.d.ts'
-            // deep: true
-        }),
-        cesium()
-    ],
-    resolve: {
-        alias: {
-            '@': path.resolve(__dirname, './src')
-        }
+import viteBaseConfig from './vite.base.config'
+import viteDevConfig from './vite.dev.config'
+import viteProdConfig from './vite.prod.config'
+
+const envResolver = {
+    build: () => {
+        console.log('生产环境')
+        return { ...viteBaseConfig, ...viteProdConfig }
     },
-    server: {
-        port: 8000,
-        open: true,
-        proxy: {
-            // 开发环境代理配置
-            [config.VITE_APP_BASE_URL + '/api']: {
-                target: 'https://mock.apifox.cn/m1/1566639-0-default',
-                changeOrigin: true,
-                rewrite: (serverPath) =>
-                    serverPath.replace(config.VITE_APP_BASE_URL + '/api', '')
-            },
-            [config.VITE_APP_BASE_URL + '/jhApi']: {
-                target: 'http://v.juhe.cn',
-                changeOrigin: true,
-                rewrite: (serverPath) =>
-                    serverPath.replace(config.VITE_APP_BASE_URL + '/jhApi', '')
-            },
-            [config.VITE_APP_BASE_URL + '/map']: {
-                target: 'https://restapi.amap.com',
-                changeOrigin: true,
-                rewrite: (serverPath) =>
-                    serverPath.replace(config.VITE_APP_BASE_URL + '/map', '')
-            }
-        }
+    serve: () => {
+        console.log('开发环境')
+        return Object.assign(viteBaseConfig, viteDevConfig)
     }
+}
+
+export default defineConfig(({ command, mode }) => {
+    console.log(mode)
+
+    return envResolver[command]()
 })
