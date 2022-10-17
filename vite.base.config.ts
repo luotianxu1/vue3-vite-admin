2fd9c4e98bbaa2e20052b00b3082b23601070409
Luotianxu <1265062404@qq.com>
2022-10-17 14:32:53 +0800

    style: vite

--- vite.base.config.ts
+++ vite.base.config.ts
@@ -1,3 +1,36 @@
 import { defineConfig } from 'vite'
+import path from 'path'
+import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import cesium from 'vite-plugin-cesium'
 
-export default defineConfig({})
+export default defineConfig({
+    plugins: [
+        vue(),
+        AutoImport({
+            resolvers: [ElementPlusResolver()],
+            imports: ['vue', 'vue-router'],
+            dts: 'src/types/auto-imports.d.ts'
+        }),
+        Components({
+            resolvers: [ElementPlusResolver()],
+            // dirs: ['src/components'],
+            // directoryAsNamespace: true,
+            dts: 'src/types/components.d.ts'
+            // deep: true
+        }),
+        cesium()
+    ],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, 'src'),
+            '@assets': path.resolve(__dirname, 'src/assets')
+        }
+    },
+    optimizeDeps: {
+        exclude: [] // 将指定数组中的依赖不进行依赖预构建
+    }
+    // envPrefix: 'ENV_' // 配置vite注入客户端环境变量校验前缀
+})
--- vite.prod.config.ts
+++ vite.prod.config.ts
@@ -1,61 +1,10 @@
-import { defineConfig, loadEnv } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import AutoImport from 'unplugin-auto-import/vite'
-import Components from 'unplugin-vue-components/vite'
-import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
-import path from 'path'
-import cesium from 'vite-plugin-cesium'
-
-const config = loadEnv('development', './')
+import { defineConfig } from 'vite'
 
 export default defineConfig({
-    plugins: [
-        vue(),
-        AutoImport({
-            resolvers: [ElementPlusResolver()],
-            imports: ['vue', 'vue-router'],
-            dts: 'src/types/auto-imports.d.ts'
-        }),
-        Components({
-            resolvers: [ElementPlusResolver()],
-            // dirs: ['src/components'],
-            // directoryAsNamespace: true,
-            dts: 'src/types/components.d.ts'
-            // deep: true
-        }),
-        cesium()
-    ],
-    resolve: {
-        alias: {
-            '@': path.resolve(__dirname, './src')
-        }
-    },
-    server: {
-        port: 8000,
-        open: true,
-        proxy: {
-            // 开发环境代理配置
-            [config.VITE_APP_BASE_URL + '/api']: {
-                target: 'https://mock.apifox.cn/m1/1566639-0-default',
-                changeOrigin: true,
-                rewrite: (serverPath) =>
-                    serverPath.replace(config.VITE_APP_BASE_URL + '/api', '')
-            },
-            [config.VITE_APP_BASE_URL + '/jhApi']: {
-                target: 'http://v.juhe.cn',
-                changeOrigin: true,
-                rewrite: (serverPath) =>
-                    serverPath.replace(config.VITE_APP_BASE_URL + '/jhApi', '')
-            },
-            [config.VITE_APP_BASE_URL + '/map']: {
-                target: 'https://restapi.amap.com',
-                changeOrigin: true,
-                rewrite: (serverPath) =>
-                    serverPath.replace(config.VITE_APP_BASE_URL + '/map', '')
-            }
-        }
-    },
-    optimizeDeps: {
-        exclude: [] // 将指定数组中的依赖不进行依赖预构建
+    build: {
+        rollupOptions: {
+            output: {}
+        },
+        assetsInlineLimit: 4096
     }
 })
