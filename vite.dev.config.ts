307445b6b7c4fddb1477fd0d86a249ab16c4d049
Luotianxu <1265062404@qq.com>
2022-10-17 16:46:50 +0800

    feat: 生产环境mock

--- vite.dev.config.ts
+++ vite.dev.config.ts
@@ -1,31 +1,30 @@
 import { defineConfig } from 'vite'
-
 // const config = loadEnv('development', './')
 
 export default defineConfig({
-    // server: {
-    //     port: 8000,
-    //     open: true,
-    //     proxy: {
-    //         // 开发环境代理配置
-    //         [config.VITE_APP_BASE_URL + '/api']: {
-    //             target: 'https://mock.apifox.cn/m1/1566639-0-default',
-    //             changeOrigin: true,
-    //             rewrite: (serverPath) =>
-    //                 serverPath.replace(config.VITE_APP_BASE_URL + '/api', '')
-    //         },
-    //         [config.VITE_APP_BASE_URL + '/jhApi']: {
-    //             target: 'http://v.juhe.cn',
-    //             changeOrigin: true,
-    //             rewrite: (serverPath) =>
-    //                 serverPath.replace(config.VITE_APP_BASE_URL + '/jhApi', '')
-    //         },
-    //         [config.VITE_APP_BASE_URL + '/map']: {
-    //             target: 'https://restapi.amap.com',
-    //             changeOrigin: true,
-    //             rewrite: (serverPath) =>
-    //                 serverPath.replace(config.VITE_APP_BASE_URL + '/map', '')
-    //         }
-    //     }
-    // }
+    server: {
+        port: 8000,
+        open: true
+        // proxy: {
+        //     // 开发环境代理配置
+        //     [config.VITE_APP_BASE_URL + '/api']: {
+        //         target: 'https://mock.apifox.cn/m1/1566639-0-default',
+        //         changeOrigin: true,
+        //         rewrite: (serverPath) =>
+        //             serverPath.replace(config.VITE_APP_BASE_URL + '/api', '')
+        //     },
+        //     [config.VITE_APP_BASE_URL + '/jhApi']: {
+        //         target: 'http://v.juhe.cn',
+        //         changeOrigin: true,
+        //         rewrite: (serverPath) =>
+        //             serverPath.replace(config.VITE_APP_BASE_URL + '/jhApi', '')
+        //     },
+        //     [config.VITE_APP_BASE_URL + '/map']: {
+        //         target: 'https://restapi.amap.com',
+        //         changeOrigin: true,
+        //         rewrite: (serverPath) =>
+        //             serverPath.replace(config.VITE_APP_BASE_URL + '/map', '')
+        //     }
+        // }
+    }
 })
--- vite.prod.config.ts
+++ vite.prod.config.ts
@@ -1,8 +1,27 @@
 import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import cesium from 'vite-plugin-cesium'
 import { viteMockServe } from 'vite-plugin-mock'
 
 export default defineConfig({
     plugins: [
+        vue(),
+        AutoImport({
+            resolvers: [ElementPlusResolver()],
+            imports: ['vue', 'vue-router'],
+            dts: 'src/types/auto-imports.d.ts'
+        }),
+        Components({
+            resolvers: [ElementPlusResolver()],
+            // dirs: ['src/components'],
+            // directoryAsNamespace: true,
+            dts: 'src/types/components.d.ts'
+            // deep: true
+        }),
+        cesium(),
         viteMockServe({
             // 更多配置见最下方
             mockPath: './mock/', //mock文件地址
@@ -10,7 +29,7 @@ export default defineConfig({
             prodEnabled: true, // 生产打包开关 // 这样可以控制关闭mock的时候不让mock打包到最终代码内
             injectCode: ` import { setupProdMockServer } from './mockProdServer'; setupProdMockServer(); `,
             logger: false, //是否在控制台显示请求日志
-            supportTs: false //打开后，可以读取 ts 文件模块。 请注意，打开后将无法监视.js 文件
+            supportTs: true //打开后，可以读取 ts 文件模块。 请注意，打开后将无法监视.js 文件
         })
     ],
     build: {
