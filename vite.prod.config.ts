565dac79847b5122c81861e81887426a71a728d8
Luotianxu <1265062404@qq.com>
2022-12-16 15:53:26 +0800

    fix: mock

--- vite.prod.config.ts
+++ vite.prod.config.ts
@@ -28,7 +28,7 @@ export default defineConfig({
             mockPath: './mock/', //mock文件地址
             localEnabled: false, // 开发打包开关
             prodEnabled: true, // 生产打包开关 // 这样可以控制关闭mock的时候不让mock打包到最终代码内
-            injectCode: ` import { setupProdMockServer } from './mockProdServer'; setupProdMockServer(); `,
+            injectCode: ` import { setupProdMockServer } from './utils/mockProdServer'; setupProdMockServer(); `,
             logger: false, //是否在控制台显示请求日志
             supportTs: true //打开后，可以读取 ts 文件模块。 请注意，打开后将无法监视.js 文件
         }),
@@ -46,11 +46,7 @@ export default defineConfig({
                 // 分包
                 manualChunks(id) {
                     if (id.includes('node_modules')) {
-                        return id
-                            .toString()
-                            .split('node_modules/')[1]
-                            .split('/')[0]
-                            .toString()
+                        return id.toString().split('node_modules/')[1].split('/')[0].toString()
                     }
                 }
             }
