3da7f93d97816137897e5ed7b3f2eae494957baf
luotianxu <1265062404@qq.com>
2023-01-17 13:47:02 +0800

    fix: elementplus 版本

--- src/types/auto-imports.d.ts
+++ src/types/auto-imports.d.ts
@@ -9,6 +9,7 @@ declare global {
   const effectScope: typeof import('vue')['effectScope']
   const EffectScope: typeof import('vue')['EffectScope']
   const ElMessage: typeof import('element-plus/es')['ElMessage']
+  const ElMessageBox: typeof import('element-plus/es')['ElMessageBox']
   const ElNotification: typeof import('element-plus/es')['ElNotification']
   const getCurrentInstance: typeof import('vue')['getCurrentInstance']
   const getCurrentScope: typeof import('vue')['getCurrentScope']
